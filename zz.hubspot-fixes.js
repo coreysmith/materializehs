2c35b478a3c37c1ed29c14dbf81d908f613bea0f
coreysmith <smith@tributemedia.com>
2016-02-13 20:48:26 -0700

    lots of theming work.

--- zz.hubspot-fixes.js
+++ zz.hubspot-fixes.js
@@ -1,6 +1,6 @@
 (function($){        
 $(document).ready(function() {
         $('#hs_cos_wrapper_hero_banner .caption a').addClass('button waves-light btn waves-effect');
-       
+        $('.hs-button').addClass('button waves-light btn waves-effect');
 });
 })(jQuery);
